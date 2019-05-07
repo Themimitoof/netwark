877000b19d97e7e902d20542d6dab82a21c0cee3
Michael Vieira <michael.vieira@gandi.net>
2019-05-08 01:20:09 +0200

    Create first version of custom types (need tests)
    #16

---
+++ netwark/models/types.py
@@ -0,0 +1,93 @@
+import sys
+import re
+import logging
+import uuid
+import time
+
+from datetime import datetime
+import json
+
+from sqlalchemy import Column, Integer, DateTime, String, engine_from_config
+from sqlalchemy.dialects.postgresql import (
+    UUID as PgUUID,
+    JSON as PgJSON,
+    JSONB as PgJSONB,
+    ARRAY as PgARRAY,
+)
+from sqlalchemy.types import (
+    TypeDecorator,
+    Concatenable,
+    UserDefinedType,
+    CHAR,
+    TEXT,
+    UnicodeText,
+)
+
+if sys.version_info[0] > 2:
+    basestring = (str, bytes)
+
+
+class UUID(TypeDecorator):
+    """
+    Platform-independent GUID type.
+
+    Uses Postgresql's UUID type, otherwise uses CHAR(32), storing as
+    stringified hex values.
+    """
+
+    impl = CHAR
+
+    def load_dialect_impl(self, dialect):
+        if dialect.name == 'postgresql':
+            return dialect.type_descriptor(PgUUID(False))
+        else:
+            return dialect.type_descriptor(CHAR(36))
+
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return value
+
+        if isinstance(value, basestring):
+            value = uuid.UUID(value)
+        elif not isinstance(value, uuid.UUID):
+            raise ValueError('%r is not a valid UUID.' % value)
+
+        # mysql should be value.bytes if BINARY
+        return str(value)
+
+    def process_result_value(self, value, dialect):
+        # mysql should convert bytes in uuid if BINARY
+        return value
+
+
+class JSON(TypeDecorator):
+    """
+    Platform-independent JSON type.
+
+    Uses Postgresql's JSON type, otherwise uses TEXT, storing as
+    stringified hex values.
+    """
+
+    impl = TEXT
+
+    def load_dialect_impl(self, dialect):
+        if dialect.name == 'postgresql':
+            return dialect.type_descriptor(PgJSON(False))
+        else:
+            return dialect.type_descriptor(TEXT())
+
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return value
+
+        if isinstance(value, basestring):
+            value = json.load(value)
+        elif not isinstance(value, json):
+            raise ValueError('%r is not a valid JSON format.' % value)
+
+        # mysql should be value.bytes if BINARY
+        return str(value)
+
+    def process_result_value(self, value, dialect):
+        # mysql should convert bytes in uuid if BINARY
+        return value
