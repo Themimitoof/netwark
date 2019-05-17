0b8ab29421d018fc494805735efde2f1c29ab2d4
Michael Vieira <contact@mvieira.fr>
2019-05-17 13:59:52 +0200

    Create the list page for returning last 50 opers
    Closes #17

--- netwark/views/operations.py
+++ netwark/views/operations.py
@@ -1,16 +1,46 @@
 from pyramid.view import view_config
 from pyramid.response import Response
 
+from ..models import Operation
+
 
 @view_config(
     route_name="operations_list", renderer="../templates/operations/list.pug"
 )
 def operations_list_view(request):
-    return {}
+    """
+    Return a list of operations. By default, it return the lasts 50 operations
+    and can be changed by using filters.
+
+    TODO (next release): Create filters for this route.
+    """
+
+    session = request.dbsession
+
+    query = (
+        session.query(Operation)
+        .order_by(Operation.updated_at.desc())
+        .limit(50)
+    )
+    operations = []
+
+    for operation in query:
+        operation = operation.to_dict()
+
+        if len(operation['queues']) > 3:
+            operation['queues'] = operation['queues'][:3]
+            operation['queues'].append("+")
+
+        operations.append(operation)
+
+    return {
+        'siteOptions': {'pageTitle': 'List of operations'},
+        'data': operations,
+    }
 
 
 @view_config(
     route_name="operations_info", renderer="../templates/operations/info.pug"
 )
 def operations_info_view(request):
-    return {}
+    return {'siteOptions': {'pageTitle': 'Overview'}, 'data': {}}
